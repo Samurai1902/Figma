83d66655593edcf70c86270086a076d1a3fd588c
Никита Дележов <nikitadel1902@vk.com>
2024-03-06 02:10:00 +0300

    update CSS, HTML, JS

--- JavaScript/script.js
+++ JavaScript/script.js
@@ -29,7 +29,7 @@ window.addEventListener('scroll', function() {
 });
 
 
-// всплывающее окно
+// tarif
 document.querySelector('.app__select-rate').addEventListener('click', function(){
   this.classList.toggle('open');
   var subnav_wrapper = document.querySelector('.app__subnav-rate-wrapper');
@@ -43,8 +43,6 @@ document.querySelector('.app__select-rate').addEventListener('click', function()
   document.querySelector('.app__select-arrow').classList.toggle('arrow-active');
 });
 
-
-
 function TarifRate(element){
   var rate = document.getElementById(element).innerText;
   var rate_wrapper = document.querySelector('.app__subnav-rate-wrapper');
@@ -54,7 +52,36 @@ function TarifRate(element){
   document.getElementById('app__select-rate-text').innerText = rate;
 };
 
-// Открытие\Закрытие окна (кнопки)
+// валидаия(не робит)
+// const phoneNumberInput = document.getElementById('phone');
+// const emailInput = document.getElementById('email');
+
+// function validatePhoneNumber(phoneNumber) {
+//   const regex = /^[0-9]{10}$/;
+//   return regex.test(phoneNumber);
+// }
+
+// function validateEmail(email) {
+//   const regex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+//   return regex.test(email);
+// }
+
+// phoneNumberInput.addEventListener('blur', function() {
+//   if (!validatePhoneNumber(phoneNumberInput.value)) {
+//     // alert('Please enter a valid phone number (e.g. 1234567890)');
+//     phoneNumberInput.value = '';
+//   }
+//   if (!validateEmail(emailInput.value)) {
+//     // alert('Please enter a valid email address (e.g. example@mail.com)');
+//     emailInput.value = '';
+//   }
+// });
+
+// emailInput.addEventListener('blur', function() {
+  
+// });\\
+
+// Открытие\Закрытие окна заявки(кнопки)
 function closePopUp() {
   document.querySelector('.app').classList.toggle('called')
   document.querySelector('.header').classList.toggle('hide')
@@ -64,15 +91,19 @@ function openPopUp() {
   document.querySelector('.header').classList.toggle('hide')
 }
 
-// Открытие окна благодарности(робит но не полностью)
+// Открытие\Закрытие окна благодарности
 function openThx() {
   document.querySelector('.app-thx').classList.toggle('called')
-  document.querySelector('.app').classList.toggle('hide')
-  document.querySelector('.header').classList.toggle('hide')
+  document.querySelector('.header').classList.toggle('called')
 }
 function closeThxPopUp() {
-  document.querySelector('.app-thx').classList.toggle('hide')
+  document.querySelector('.app-thx').classList.toggle('called')
+  document.querySelector('.header').classList.toggle('hide')
   document.querySelector('.app').classList.toggle('called')
-  document.querySelector('.header').classList.toggle('called')
+}
+function Send() {
+  document.querySelector('.app-thx').classList.toggle('called');
+  document.querySelector('.app').classList.toggle('called');
+  document.querySelector('.header').classList.toggle('hide')
 }
 
